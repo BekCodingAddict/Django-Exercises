46b1d4732fe2f8bc9745c3f794572b880b0c8f10
BekCodingAddict <experte001@gmail.com>
2025-11-29 22:49:26 +0900

    Creating Url,DB Model and Render Templates🚀

--- Intro/base/views.py
+++ Intro/base/views.py
@@ -1,9 +1,25 @@
 # Create your views here.
 from django.shortcuts import render
-from django.http import HttpResponse
+
+rooms=[
+    {'id': 1, 'name': 'Lets learn Python', 'description': 'This is a room for learning Python!'},
+    {'id': 2, 'name': 'Lets learn Django', 'description': 'This is a room for learning Django!'},
+    {'id': 3, 'name': 'Lets learn JavaScript', 'description': 'This is a room for learning JavaScript!'},
+]
 
 def home(request):
-    return HttpResponse("<h1 style='color: red;'>Homepage</h1>")
+    context = {
+        'rooms': rooms
+    }
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return HttpResponse("<h1 style='color: blue;'>Room</h1>")
+def room(request, pk):
+    room = None
+    for r in rooms:
+        if r['id'] == int(pk):
+            room = r
+            break
+    context = {
+        'room': room
+    }
+    return render(request, 'base/room.html', context)
